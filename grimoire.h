628d426de952c19b206e97f71bbdc5cb564dccbe
aChosenUndead <tito_boyy@hotmail.com>
2017-06-13 17:40:04 -0300

    Add files via upload

---
+++ grimoire.h
@@ -0,0 +1,23 @@
+/**
+* @file grimoire.h
+* @author Vinícius da Costa de Andrade <andradecostavinicius@gmail.com>
+*/
+
+#ifndef GRIMOIRE_H
+#define GRIMOIRE_H
+
+#include <fstream>
+
+class Grimoire {
+	private:
+		int quantity;
+	public:
+		Grimoire ();
+		Monster* get(int i);
+		void InsertMonster(Monster* monster);
+		void RemoveMonster(Monster* Monster);
+		bool isEmpty();
+		int getQuantity();
+};
+
+#endif
